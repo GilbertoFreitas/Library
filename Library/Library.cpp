77b169e9f3da03e41aa3e4c6f74d5f676be5fdc7
Gilberto Freitas <gilbertodefreitasm@gmail.com>
2021-02-07 14:17:03 -0300

    Feito: - Criadas as classes CreateAccount e Login. (Library.cpp) - Alterações feitas em DisplayMainMenu. (Library.cpp) - Adicionado o menu de login na Main(). - Adicionada a classe User e o cabeçalho Role.

--- Library/Library.cpp
+++ Library/Library.cpp
@@ -2,20 +2,104 @@
 #include "Book.h"
 #include "Inventory.h"
 #include <string>
+#include "User.h"
 
 using namespace std;
 
 Inventory _inventory;
+vector<User> _users;
+User _loggedInUser;
+
+void CreateAccount()
+{
+
+    User newUser;
+    /*cout << "First Name:";
+    string firstName;
+    getline(cin, firstName);
+    cout << endl;
+
+    cout << "Last Name: ";
+    string lastName;
+    getline(cin, lastName);
+    cout << endl;*/
+
+    cout << "Username: ";
+    getline(cin, newUser.Username);
+    cout << endl;
+
+    cout << "Choose a role: " << endl;
+    cout << "1 - Admin " << endl;
+    cout << "2 - Employee " << endl;
+    cout << "3 - Member " << endl;
+
+    int roleOption;
+
+    cin >> roleOption;
+    cin.ignore();
+
+    if (roleOption == 1)
+    {
+        newUser.Role = Role::Admin;
+    }
+    else if (roleOption == 2)
+    {
+        newUser.Role = Role::Employee;
+    }
+    else
+    {
+        newUser.Role = Role::Member;
+    }
+
+
+    _users.push_back(newUser);
+
+}
+
+void Login()
+{
+    cout << "Choose and option:" << endl;
+    cout << "1 - Log In" << endl;
+    cout << "2 - Create account" << endl;
+
+    int option;
+    cin >> option;
+    cin.ignore();
+
+    if (option == 2)
+    {
+        CreateAccount();
+    }
+
+    cout << "Enter username: ";
+    string username;
+    cin >> username;
+
+    User user;
+    user.Username = username;
+
+    vector<User>::iterator it = find(_users.begin(), _users.end(), user);
+
+    if (it != _users.end())
+    {
+        _loggedInUser = _users[it - _users.begin()];
+    }
+}
 
 void DisplayMainMenu()
 {
+    cout << endl;
     cout << "Choose and option:" << endl;
-    cout << "1 - Add book" << endl;
+    if (_loggedInUser.Role == Role::Employee || _loggedInUser.Role == Role::Admin)
+    {
+        cout << "1 - Add book" << endl;
+        cout << "5 - Remove book from library" << endl;
+        cout << "6 - List all checked out books" << endl;
+    }
+    
     cout << "2 - List All books" << endl;
     cout << "3 - Check out book" << endl;
     cout << "4 - Check in book" << endl;
-    cout << "5 - Remove book from library" << endl;
-    cout << "6 - List all checked out books" << endl;
 
     cout << "0 - Exit" << endl;
 }
@@ -97,64 +181,65 @@ void DisplayCheckedOutBooks()
 
 int main()
 {
-    while (true)
-    {
-        DisplayMainMenu();
-
-        int input;
-
-        cin >> input;
-        cin.ignore();
-        switch (input)
+    Login();
+        while (true)
         {
-        case 0:
+            DisplayMainMenu();
 
-            cout << "Thank you. Good bye!" << endl;
-            return 0;
+            int input;
 
-        case 1:
-        {
-            AddNewBook();
-            break;
+            cin >> input;
+            cin.ignore();
+            switch (input)
+            {
+            case 0:
+
+                cout << "Thank you. Good bye!" << endl;
+                return 0;
+
+            case 1:
+            {
+                AddNewBook();
+                break;
+            }
+
+            case 2:
+            {
+                ListBooks();
+                break;
+            }
+
+            case 3:
+            {
+                CheckInOrOutBook(true);
+                break;
+            }
+
+            case 4:
+            {
+                CheckInOrOutBook(false);
+                break;
+            }
+
+            case 5:
+            {
+                RemoveBook();
+                break;
+            }
+
+            case 6:
+            {
+                DisplayCheckedOutBooks();
+                break;
+            }
+
+            default:
+                cout << "Invalid selection. Try again." << endl;
+                break;
+            }
+
+
         }
-
-        case 2:
-        {
-            ListBooks();
-            break;
-        }
-
-        case 3:
-        {
-            CheckInOrOutBook(true);
-            break;
-        }
-
-        case 4:
-        {
-            CheckInOrOutBook(false);
-            break;
-        }
-
-        case 5:
-        {
-            RemoveBook();
-            break;
-        }
-
-        case 6:
-        {
-            DisplayCheckedOutBooks();
-            break;
-        }
-
-        default:
-            cout << "Invalid selection. Try again." << endl;
-            break;
-        }
-        
-
-    }
 
     return 0;
 
